d0e86b1b1fa045fd85e328ae325b2da32ccdcf13
goliatone <burgosemi@gmail.com>
2013-12-27 23:13:42 -0500

    main.js: Added foundation shim.

--- app/main.js
+++ app/main.js
@@ -3,13 +3,21 @@
 requirejs.config({
     paths: {
         'jquery': 'components/jquery/jquery',
+        'foundation':'assets/js/foundation',
         'FlatG': 'FlatG'
+    },
+    shim:{
+    	'foundation':{
+            deps:['jquery']
+        }
     }
 });
 
-define(['FlatG', 'jquery'], function (FlatG, $) {
+define(['FlatG', 'jquery', 'foundation'], function (FlatG, $) {
     console.log('Loading');
 
+    $(document).foundation();
+    
 	var FlatG = new FlatG();
 	FlatG.init();
 });
